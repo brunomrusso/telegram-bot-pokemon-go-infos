aafd3ef6e3c22c136d92dc7d498347d516ec6fb7
brunomrusso <brunopiuu@hotmail.com>
2021-05-05 15:27:31 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ class TelegramBot():
 
  #Obter mensagens
   def obter_mensagens(self, update_id):
-    link_requisicao = f'{self.url_base}getUpdates?timeout=100'
+    link_requisicao = f'{self.url_base}getUpdates?timeout=500'
     if update_id:
       link_requisicao = f'{link_requisicao}&offset={update_id + 1}'
     resultado = requests.get(link_requisicao)
